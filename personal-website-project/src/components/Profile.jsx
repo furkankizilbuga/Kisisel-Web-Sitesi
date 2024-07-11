5ab95513bd11ae08c175516201e6f63200387794
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-07-11 20:55:38 +0300

    profile grid added

--- personal-website-project/src/components/Profile.jsx
+++ personal-website-project/src/components/Profile.jsx
@@ -12,17 +12,17 @@ export default function Profile() {
     return (
         <section className="profile border-t-2 border-[#BAB2E7]">
             <h2 className="text-title text-5xl font-semibold pt-11 dark:text-[#AEBCCF]">{profile.header}</h2>
-            <div className="flex gap-6 pt-5">
-                <div className="profile-box">
-                    <h3 className="text-main-purple font-medium text-3xl dark:text-[#B7AAFF]">{title}</h3>
-                    <div className="flex gap-2">
-                        <div>
+            <div className="flex gap-32 pt-5">
+                <div className="profile-box min-w-64">
+                    <h3 className="text-main-purple pb-4 font-medium text-3xl dark:text-[#B7AAFF]">{title}</h3>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="grid">
                             <p className="font-semibold dark:text-white">{birth}</p>
                             <p className="font-semibold dark:text-white">{residence}</p>
                             <p className="font-semibold dark:text-white">{education}</p>
                             <p className="font-semibold dark:text-white">{preferred}</p>
                         </div>
-                        <div>
+                        <div  className="grid">
                             <p className="dark:text-white">{myBirth}</p>
                             <p className="dark:text-white">{myResidence}</p>
                             <p className="dark:text-white">{myEducation}</p>
@@ -31,7 +31,7 @@ export default function Profile() {
                     </div>
                 </div>
                 <div className="about-box">
-                    <h3 className="text-main-purple font-medium text-3xl dark:text-[#B7AAFF]">{about.title}</h3>
+                    <h3 className="text-main-purple pb-4 font-medium text-3xl dark:text-[#B7AAFF]">{about.title}</h3>
                     <div>
                         <p className="text-main-light-grey text-lg dark:text-white">
                             {about.description}
--- personal-website-project/src/components/Skills.jsx
+++ personal-website-project/src/components/Skills.jsx
@@ -10,7 +10,7 @@ export default function Skills() {
             <h2 className="text-title text-5xl font-semibold dark:text-[#AEBCCF]">{skills.header}</h2>
             <div className="flex gap-32 pt-5">
                 <div>
-                    <h3 className="text-3xl text-main-purple font-medium dark:text-[#B7AAFF]">{skills.javaScript.title}</h3>
+                    <h3 className="text-3xl text-main-purple pb-4 font-medium dark:text-[#B7AAFF]">{skills.javaScript.title}</h3>
                     <p className="text-main-light-grey dark:text-white">
                         {skills.javaScript.description}
                     </p>
