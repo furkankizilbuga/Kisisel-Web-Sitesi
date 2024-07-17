848fd60bcc5d736ba8544b254d1e639699c045b4
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-07-17 18:41:48 +0300

    Update Hero.jsx

--- personal-website-project/src/components/Hero.jsx
+++ personal-website-project/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { TextContext } from "../contexts/TextContext";
 
 export default function Hero() {
 
-    const { handleScroll, bottomRef, data } = useContext(TextContext)
+    const { handleScroll, footerRef, data } = useContext(TextContext)
 
     
 
@@ -19,7 +19,7 @@ export default function Hero() {
                     {description}
                 </p>
                 <div className="flex gap-3">
-                    <button onClick={() => handleScroll(bottomRef)} className="text-main-purple bg-transparent transition border-main-purple hover:bg-main-purple hover:text-white dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF] dark:hover:bg-[#E1E1FF] dark:hover:text-black">{hire}</button>
+                    <button onClick={() => handleScroll(footerRef)} className="text-main-purple bg-transparent transition border-main-purple hover:bg-main-purple hover:text-white dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF] dark:hover:bg-[#E1E1FF] dark:hover:text-black">{hire}</button>
                     <a href="https://github.com/furkankizilbuga" target="_blank" className="transition cursor-pointer flex items-center border rounded-lg font-medium px-5 text-main-purple bg-transparent border-main-purple hover:bg-main-purple hover:text-white dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF] dark:hover:bg-[#E1E1FF] dark:hover:text-black"><i className="dark:text-[#BAB2E7]" />Github</a>
                     <a href="https://www.linkedin.com/in/furkan-k%C4%B1z%C4%B1lbu%C4%9Fa-8b0bb8278/" target="_blank" className="transition cursor-pointer flex items-center border rounded-lg font-medium px-5 text-main-purple bg-transparent border-main-purple hover:bg-main-purple hover:text-white dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF] dark:hover:bg-[#E1E1FF] dark:hover:text-black"><i className="dark:text-[#BAB2E7]" />Linkedin</a>
                 </div>
