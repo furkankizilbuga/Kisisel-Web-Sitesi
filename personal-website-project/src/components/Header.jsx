b86b1e99f884647976ef4a122b53e04579f0effc
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-07-17 19:27:15 +0300

    Update Header.jsx

--- personal-website-project/src/components/Header.jsx
+++ personal-website-project/src/components/Header.jsx
@@ -60,9 +60,9 @@ export default function Header() {
             <div className="flex justify-between items-center mt-5">
                 <p className="rounded-full p-3 bg-[#EEEBFF] text-[#7B61FF] font-bold inline-flex items-center justify-center w-12 h-12 dark:bg-[#4731D3] dark:text-[#8F88FF]">F</p>
                 <div className="flex gap-3">
-                    <button onClick={() => handleScroll(skillsRef)} className="text-main-light-grey px-7 bg-transparent hover:bg-main-purple hover:text-white dark:hover:border-white dark:hover:bg-transparent dark:text-[#6B7280] max-[700px]:text-sm">{skills}</button>
-                    <button onClick={() => handleScroll(projectsRef)} className="text-main-light-grey px-7 bg-transparent hover:bg-main-purple hover:text-white dark:hover:border-white dark:hover:bg-transparent dark:text-[#6B7280] max-[700px]:text-sm">{projects}</button>
-                    <button onClick={() => handleScroll(footerRef)} className="bg-transparent px-7 text-main-purple border-main-purple hover:transition hover:bg-main-purple hover:text-white dark:bg-white dark:hover:bg-main-purple max-[700px]:text-sm">{hire}</button>
+                    <button onClick={() => handleScroll(skillsRef)} className="text-main-light-grey px-7 bg-transparent hover:bg-main-purple hover:text-white dark:hover:border-white dark:hover:bg-transparent dark:text-[#6B7280] max-[700px]:text-sm max-[700px]:px-4">{skills}</button>
+                    <button onClick={() => handleScroll(projectsRef)} className="text-main-light-grey px-7 bg-transparent hover:bg-main-purple hover:text-white dark:hover:border-white dark:hover:bg-transparent dark:text-[#6B7280] max-[700px]:text-sm max-[700px]:px-4">{projects}</button>
+                    <button onClick={() => handleScroll(footerRef)} className="bg-transparent px-7 text-main-purple border-main-purple hover:transition hover:bg-main-purple hover:text-white dark:bg-white dark:hover:bg-main-purple max-[700px]:text-sm max-[700px]:px-4">{hire}</button>
                 </div>
             </div>
         </header>
