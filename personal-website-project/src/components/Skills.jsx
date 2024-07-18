154db7ed405461f53706c7dd403a838d32f64676
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-07-18 10:13:14 +0300

    Update Skills.jsx

--- personal-website-project/src/components/Skills.jsx
+++ personal-website-project/src/components/Skills.jsx
@@ -14,7 +14,7 @@ export default function Skills() {
             <div className="lg:flex lg:justify-between gap-32 pt-5">
                 {skillArr.map((skill, index) => {
                     return (
-                        <div key={index} className="min-w-44 max-w-sm">
+                        <div key={index} className="min-w-44 max-w-sm pt-3">
                             <h3 className="text-3xl text-main-purple pb-4 font-medium dark:text-[#B7AAFF]">{skill.title}</h3>
                             <p className="text-main-light-grey dark:text-white">
                                 {skill.description}
